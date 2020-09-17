52586c3fec2b471a52f127030cbd54c60a5b58d1
jiangyuwen <>
2020-09-17 10:10:23 +0800

    还原jar方式打包

--- src/main/java/cn/luischen/MySiteApplication.java
+++ src/main/java/cn/luischen/MySiteApplication.java
@@ -3,21 +3,14 @@ package cn.luischen;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("cn.luischen.dao")
 @EnableCaching
-public class MySiteApplication extends SpringBootServletInitializer {
+public class MySiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySiteApplication.class, args);
 	}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(MySiteApplication.class);
-    }
 }
